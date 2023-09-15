c45bc833a7010d3686b951511700c06e82171eb2
Amit Singh <theamitsingh21@gmail.com>
2023-09-15 13:59:23 +0530

    Completed Javascripts Objects and Arrays. Also learn nested Objects, Arrays and Combinations

---
+++ Javascript/Manipulating Complex Objects.js
@@ -0,0 +1,24 @@
+const myMusic = [
+    {
+      "artist": "Billy Joel",
+      "title": "Piano Man",
+      "release_year": 1973,
+      "formats": [
+        "CD",
+        "8T",
+        "LP"
+      ],
+      "gold": true
+    }
+  ];
+  const newAlbum = {
+    "artist" : "Lata Mangeskar",
+    "title": "Kuchh Kuchh",
+    "release_year": 1988,
+    "formats": [
+      "CD",
+      "8T",
+      "LP"
+    ]
+  };
+  myMusic.push(newAlbum);
