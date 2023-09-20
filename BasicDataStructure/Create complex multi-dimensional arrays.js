3e938405336bc111889e85e3c1c7c268c58dcde3
Amit Singh <theamitsingh21@gmail.com>
2023-09-21 00:50:56 +0530

    Basic Data Structure Completed

---
+++ BasicDataStructure/Create complex multi-dimensional arrays.js
@@ -0,0 +1,12 @@
+let myNestedArray = [
+    // Only change code below this line
+    "levelOne", 
+      ["secondLevel",
+        ["deep", 
+          ["deeper",
+            ["deepest"]
+          ]
+        ]
+      ]
+    // Only change code above this line
+  ];
